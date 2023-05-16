d8c1e25595f501b60a69a9ddd5b4f6e81b6874f7
Henrik Schack <hschack@gmail.com>
2023-05-16 12:47:18 +0200

    160523_1247

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,94 @@
 #include <Arduino.h>
+#include "ADS1X15.h"
 #include <Wire.h>
 
 #define I2C_SDA 4
 #define I2C_SCL 5
 
+//
+//    FILE: ADS_async_differential.ino
+//  AUTHOR: Rob.Tillaart
+// VERSION: 0.1.0
+// PURPOSE: read multiple differential continuously
+//
 
+// test
+// connect 4 potmeters
+//
+// GND ---[   x   ]------ 5V
+//            |
+//
+// measure at x  - connect to AIN0..4.
+//
+//
+// choose your sensor
+// ADS1013 ADS(0x48);
+// ADS1014 ADS(0x48);
+// ADS1015 ADS(0x48);
+// ADS1113 ADS(0x48);
+// ADS1114 ADS(0x48);
+ADS1115 ADS(0x48);
+uint8_t pair = 01;
+int16_t val_01 = 0;
+int16_t val_23 = 0;
 
-// constants won't change. Used here to set a pin number:
-const int ledPin = LED_BUILTIN;  // the number of the LED pin
+bool handleConversion(void);
 
-// Variables will change:
-int ledState = LOW;  // ledState used to set the LED
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;  // will store last time LED was updated
-
-// constants won't change:
-const long interval = 1000;  // interval at which to blink (milliseconds)
-
-void setup() 
+void setup()
 {
-  __asm__("nop\n\t");
-  // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);
-  Wire.begin(I2C_SDA, I2C_SCL, 100000ul); // I2C device found at address 0x48  !
   Serial.begin(115200);
-  while (!Serial);             // Leonardo: wait for serial monitor
-  Serial.println("\nI2C Scanner");
+  Serial.println(__FILE__);
+  Serial.print("ADS1X15_LIB_VERSION: ");
+  Serial.println(ADS1X15_LIB_VERSION);
+
+  ADS.begin(I2C_SDA, I2C_SCL); // I2C device found at address 0x48  !
+  ADS.setGain(0);        // 6.144 volt
+  ADS.setDataRate(4);    // medium
+
+  // single shot mode
+  ADS.setMode(1);
+  // start with first pair
+  pair = 01;
+  // trigger first read
+  ADS.requestADC_Differential_0_1();
 }
+
 
 void loop()
 {
-  byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
+  if (handleConversion() == true)
   {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
+    Serial.print("COMP:\t");
+    Serial.print(val_23);
+    Serial.println();
   }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(5000);           // wait 5 seconds for next 
+
+  // do other stuff here
+  delay(100);
+}
+
+
+// can be changed to hold other differentials reads too.
+bool handleConversion()
+{
+  if (ADS.isReady())
+  {
+    if (pair == 01)
+    {
+      val_01 = ADS.getValue();
+      pair = 23;
+      ADS.requestADC_Differential_2_3();
+      return false;  // only one done
+    }
+
+    // last of series to check
+    if (pair == 23)
+    {
+      val_23 = ADS.getValue();
+      pair = 01;
+      ADS.requestADC_Differential_0_1();
+      return true;   // both are updated
+    }
+  }
+  return false;  // default not all read
 }
